05161ea9e021ab7320e55f51d811f0965d2b0f77
Belkeis Mez <belkeis.meziani@univ-constantine2.dz>
2025-11-24 18:30:13 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/example/TP7_Idl/ControlerTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ControllerTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    @Order(1)
+    void shouldSaveStudent() {
+        Student student = new Student();
+        student.setName("Charlie");
+        student.setAddress("Algeria");
+        studentRepository.save(student);
+        // يتأكد من أنه تم حفظ طالب واحد بالفعل
+        assertThat(studentRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(2)
+    void shouldFindAllStudents() {
+        // يتأكد من إمكانية استرجاع الطالب
+        assertThat(studentRepository.findAll()).hasSize(1);
+    }
+}
